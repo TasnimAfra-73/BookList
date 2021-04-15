e7b181bbe68f05e16068597e0f328da4ad4ff965
ehsanrishat <erishat162079@bscse.uiu.ac.bd>
2021-04-16 00:05:15 +0600

    Data is retrieved from local storage

--- booklist.js
+++ booklist.js
@@ -5,6 +5,18 @@ const addButton = document.getElementById("add");
 const bookList = document.getElementById("tbody");
 const bookArray = [];
 
+document.addEventListener("DOMContentLoaded", function(){
+    let ui = new UI();
+    let storageData = JSON.parse(localStorage.getItem("books"));
+  if (storageData) {
+    storageData.forEach((element) => {
+      bookArray.push(element);
+      ui.displayBook(element);
+    });
+  }
+});
+
+
 function Book(title, author, isbn) {
   this.title = title;
   this.author = author;
@@ -20,6 +32,7 @@ UI.prototype.addBook = function (book) {
   let bookObj = {title: book.title, author:book.author, isbn: book.isbn};
   bookArray.push(bookObj);
   localStorage.setItem("books", JSON.stringify(bookArray));
+  console.log(bookArray);
 };
 
 UI.prototype.displayBook = function(book){
@@ -36,6 +49,15 @@ UI.prototype.clearField = function(){
     title.value = "";
     author.value = "";
     isbn.value = "";
+}
+
+UI.prototype.getBook = function(){
+    let storageData = JSON.parse(localStorage.getItem("books"));
+  if (storageData) {
+    storageData.forEach((element) => {
+      bookArray.push(element);
+    });
+  }
 }
 
 addButton.onclick = function(event){
